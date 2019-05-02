035e5d59cee81b75ebc1cd5184497321e8df43cf
Philip <philip.j.m@gmail.com>
2019-05-02 11:17:23 +0100

    Remove windows cruft

--- core/database/database.hpp
+++ core/database/database.hpp
@@ -1,15 +1,9 @@
 #include <string>
 
-/* #if defined(COMPILING_DLL)
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif */
-
 namespace core::database {
 
-void /* DLLEXPORT */ printString(const std::string& someString);
+void printString(const std::string& someString);
 
-int /* DLLEXPORT */ returnNumberTwo();
+int returnNumberTwo();
 
 } // core::database
--- core/io/io.hpp
+++ core/io/io.hpp
@@ -1,15 +1,9 @@
 #include <string>
 
-/* #if COMPILING_DLL
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif */
-
 namespace core::io {
 
-	void /* DLLEXPORT */ printString(const std::string& someString);
+	void printString(const std::string& someString);
 
-	int /* DLLEXPORT */ returnNumberFour();
+	int returnNumberFour();
 
 } // core::database
